a5616e7855174fe9c75806f1704f8c9ad892c294
your_username <your_email_address@example.com>
2021-05-01 02:25:47 +0100

    video 3, blog page logic,paging,categories count,latests post side bar

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
@@ -12,14 +14,41 @@ def index(request):
         new_signup.email = email
         new_signup.save()
         
-    context = {
+    context = { 
         'object_list': featured,
         'latest' : latest
     }
     return render(request, 'index.html', context)
 
-def blog(request):
-    return render(request, 'blog.html', {})
 
-def post(request):
+def get_category_count():
+    queryset = Post \
+        .objects \
+        .values('categories__title') \
+        .annotate(Count('categories__title'))
+    return queryset
+
+def blog(request):
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+    context = {
+        'category_count': category_count,
+        'queryset': paginated_queryset,
+        'most_recent': most_recent,
+        'page_request_var': page_request_var
+    }
+    return render(request, 'blog.html', context)
+
+def post(request, id):
     return render(request, 'post.html', {})
